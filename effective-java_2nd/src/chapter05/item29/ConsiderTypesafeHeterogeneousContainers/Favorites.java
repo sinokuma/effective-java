af3722310b31c34fc650c848e1d4ddcfcd417396
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-05-13 18:06:57 +0900

    No.29 - 型安全な異種コンテナを検討する

---
+++ effective-java_2nd/src/chapter05/item29/ConsiderTypesafeHeterogeneousContainers/Favorites.java
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+package chapter05.item29.ConsiderTypesafeHeterogeneousContainers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 型安全な異種コンテナを検討する<br>
+ * 型安全な異種コンテナパターン
+ *
+ * @author sinokuma
+ *
+ */
+public class Favorites {
+
+    private Map<Class<?>, Object> favorites = new HashMap<>();
+
+    // 動的キャストで実行時型安全性を達成する
+    public <T> void putFavorite(Class<T> type, T instance) {
+        if (type == null)
+            throw new NullPointerException("Type is null");
+        favorites.put(type, type.cast(instance));
+    }
+
+    public <T> T getFavorite(Class<T> type) {
+        return type.cast(favorites.get(type));
+    }
+}
