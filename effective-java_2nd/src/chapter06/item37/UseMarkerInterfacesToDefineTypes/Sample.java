686e7c818cd0b1f3f6bbc5ea82058a72f4aeae85
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-06-14 02:03:00 +0900

    No.37 - 型を定義するためにマーカーインタフェースを使用する

---
+++ effective-java_2nd/src/chapter06/item37/UseMarkerInterfacesToDefineTypes/Sample.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package chapter06.item37.UseMarkerInterfacesToDefineTypes;
+
+/**
+ * 型を定義するためにマーカーインタフェースを使用する<br>
+ * マーカーインタフェースは、マークされたクラスのインスタンスが実行している型を定義すること。<br>
+ * マーカーアノテーションは型を定義しない。<br>
+ * ElementType.TYPEであるマーカーアノテーションについては、<br>
+ * アノテーション型であるべきか、アーカーインタフェースであるべきか検討すること。
+ *
+ * @author sinokuma
+ *
+ */
+public class Sample {
+    // No example codes
+}
