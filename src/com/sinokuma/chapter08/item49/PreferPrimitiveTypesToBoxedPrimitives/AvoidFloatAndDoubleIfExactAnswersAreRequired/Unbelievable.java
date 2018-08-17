c036262852667455da6665fa1485148740d1d22e
sinokuma <rip.sk0207@gmail.com>
2018-08-17 16:57:44 +0900

    No.49 - ボクシングされた基本データより基本データ型を選ぶ。

---
+++ src/com/sinokuma/chapter08/item49/PreferPrimitiveTypesToBoxedPrimitives/AvoidFloatAndDoubleIfExactAnswersAreRequired/Unbelievable.java
@@ -0,0 +1,15 @@
+package com.sinokuma.chapter08.item49.PreferPrimitiveTypesToBoxedPrimitives.AvoidFloatAndDoubleIfExactAnswersAreRequired;
+
+/**
+ * ボクシングされた基本データより基本データ型を選ぶ。
+ *
+ * @author sinokuma
+ */
+public class Unbelievable {
+    static Integer i;
+
+    public static void main(String[] args) {
+        if (i == 42)
+            System.out.println("Unbelievable");
+    }
+}
