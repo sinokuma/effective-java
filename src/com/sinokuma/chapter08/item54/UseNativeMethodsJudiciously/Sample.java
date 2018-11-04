290644f7502842aba2b54de428ad4fb131c4d1a9
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-11-05 00:42:09 +0900

    fix: remove unused import

--- src/com/sinokuma/chapter08/item54/UseNativeMethodsJudiciously/Sample.java
+++ src/com/sinokuma/chapter08/item54/UseNativeMethodsJudiciously/Sample.java
@@ -1,8 +1,5 @@
 package com.sinokuma.chapter08.item54.UseNativeMethodsJudiciously;
 
-import java.util.Arrays;
-import java.util.Set;
-
 /**
  * ネイティブメソッドを注意して使用する
  * パフォーマンス改善のためにネイティブメソッドを使用することは非推奨
