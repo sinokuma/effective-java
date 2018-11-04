73f5b5a0dcde788432d6e6660efae3ea97219e33
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-11-05 00:42:30 +0900

    No.55 - 注意して最適化する

---
+++ src/com/sinokuma/chapter08/item55/OptimizeJudiciously/Sample.java
@@ -0,0 +1,14 @@
+package com.sinokuma.chapter08.item55.OptimizeJudiciously;
+
+/**
+ * 注意して最適化する
+ * 速いプログラムよりも良いプログラムを書く
+ * パフォーマンスを制限するような設計を避ける
+ * API設計によるパフォーマンスの結果を考慮する
+ * 良いパフォーマンスを達成するためにAPIをねじ曲げるのはかなり悪い考え
+ *
+ * @author sinokuma
+ */
+public class Sample {
+    // nothing to do
+}
