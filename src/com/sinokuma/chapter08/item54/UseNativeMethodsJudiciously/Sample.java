78b7f44e4e2b169ab7cb91e9fae9a7ebc65a4f70
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-11-05 00:36:26 +0900

    No.54 - ネイティブメソッドを注意して使用する

---
+++ src/com/sinokuma/chapter08/item54/UseNativeMethodsJudiciously/Sample.java
@@ -0,0 +1,15 @@
+package com.sinokuma.chapter08.item54.UseNativeMethodsJudiciously;
+
+import java.util.Arrays;
+import java.util.Set;
+
+/**
+ * ネイティブメソッドを注意して使用する
+ * パフォーマンス改善のためにネイティブメソッドを使用することは非推奨
+ * ネイティブコード中のたった1つの不具合で、アプリケーション全体をだめにできます
+ *
+ * @author sinokuma
+ */
+public class Sample {
+    // nothing to do
+}
