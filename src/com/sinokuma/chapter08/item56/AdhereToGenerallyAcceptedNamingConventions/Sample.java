aaa83f4439e06ad92dd0c257580cb688329cdc90
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-11-05 00:49:49 +0900

    No.56 - 一般的に受け入れられている命名規則を守る

---
+++ src/com/sinokuma/chapter08/item56/AdhereToGenerallyAcceptedNamingConventions/Sample.java
@@ -0,0 +1,17 @@
+package com.sinokuma.chapter08.item56.AdhereToGenerallyAcceptedNamingConventions;
+
+/**
+ * 一般的に受け入れられている命名規則を守る
+ *
+ * パッケージ：com.google.inject, org.joda.time.format
+ * クラス、インタフェース：Timer, FutureTask, LinkedHashMap, HttpServlet
+ * メソッド、フィールド：remove, ensureCapacity, getCrc
+ * 定数フィールド：VALUES, NEGATIVE_INFINITY
+ * ローカル変数：i, xref, houseNumber
+ * 型パラメータ：T, E, K, V, X, T1, T2
+ *
+ * @author sinokuma
+ */
+public class Sample {
+    // nothing to do
+}
