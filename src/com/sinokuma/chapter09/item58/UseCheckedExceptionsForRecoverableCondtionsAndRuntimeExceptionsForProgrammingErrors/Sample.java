cb23fceaef660a00a525d73d1113223c64321a8c
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-12-24 01:18:07 +0900

    No.58 - 回復可能な状態にはチェックされる例外を、プログラミングエラーには実行時例外を使用する

---
+++ src/com/sinokuma/chapter09/item58/UseCheckedExceptionsForRecoverableCondtionsAndRuntimeExceptionsForProgrammingErrors/Sample.java
@@ -0,0 +1,11 @@
+package com.sinokuma.chapter09.item58.UseCheckedExceptionsForRecoverableCondtionsAndRuntimeExceptionsForProgrammingErrors;
+
+/**
+ * 回復可能な状態にはチェックされる例外を、
+ * プログラミングエラーには実行時例外を使用する
+ *
+ * @author sinokuma
+ */
+public class Sample {
+    // nothing to do
+}
