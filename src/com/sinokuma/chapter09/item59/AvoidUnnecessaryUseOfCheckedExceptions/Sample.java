52b7ae0288522c40f40ddffcb2d14b431948f901
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-12-29 00:44:44 +0900

    No.59 - チェックされる例外を不必要に使用するのを避ける

---
+++ src/com/sinokuma/chapter09/item59/AvoidUnnecessaryUseOfCheckedExceptions/Sample.java
@@ -0,0 +1,13 @@
+package com.sinokuma.chapter09.item59.AvoidUnnecessaryUseOfCheckedExceptions;
+
+/**
+ * チェックされる例外を不必要に使用するのを避ける
+ *
+ * APIの適切な使用では例外状態を防ぐことができない、かつ、そのAPIを使用しているプログラマが例外に直面した時に
+ * 何らかの有用な処理をできる場合に正当化される。両方の条件が満たされない限り、チェックされない例外の方が適切。
+ *
+ * @author sinokuma
+ */
+public class Sample {
+    // nothing to do
+}
