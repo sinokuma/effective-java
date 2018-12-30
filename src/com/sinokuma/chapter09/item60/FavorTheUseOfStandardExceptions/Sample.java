2aed77cee3f08d975528acc4287e6ed8a0cb56ad
Sakuya Inokuma <rip.sk0207@gmail.com>
2018-12-30 14:37:53 +0900

    No.60 - 標準例外を使用する

---
+++ src/com/sinokuma/chapter09/item60/FavorTheUseOfStandardExceptions/Sample.java
@@ -0,0 +1,17 @@
+package com.sinokuma.chapter09.item60.FavorTheUseOfStandardExceptions;
+
+/**
+ * 標準例外を使用する
+ *
+ * IllegalArgumentException        : パラメータ値が不適切
+ * IllegalStateException           : メソッド呼び出しに対してオブジェクト状態が不正
+ * NullPointerException            : パラメータ値が禁止されているnull
+ * IndexOutOfBoundsException       : インデックスパラメータ値が範囲外
+ * ConcurrentModificationException : 禁止されているオブジェクトの並行した変更を検出
+ * UnsupportedOperationException   : オブジェクトがメソッドをサポートしていない
+ *
+ * @author sinokuma
+ */
+public class Sample {
+    // nothing to do
+}
