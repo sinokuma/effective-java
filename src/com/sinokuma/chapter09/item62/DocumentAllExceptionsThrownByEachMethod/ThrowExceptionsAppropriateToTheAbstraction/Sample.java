62c801d1e3b06e6f005077861ae4dc8cc2c6dce2
Sakuya Inokuma <rip.sk0207@gmail.com>
2019-03-24 23:39:42 +0900

    No.62 - 各メソッドがスローする全ての例外を文書化する

---
+++ src/com/sinokuma/chapter09/item62/DocumentAllExceptionsThrownByEachMethod/ThrowExceptionsAppropriateToTheAbstraction/Sample.java
@@ -0,0 +1,14 @@
+package com.sinokuma.chapter09.item62.DocumentAllExceptionsThrownByEachMethod.ThrowExceptionsAppropriateToTheAbstraction;
+
+/**
+ * 各メソッドがスローする全ての例外を文書化する
+ *
+ * 常にチェックされる例外を個々に宣言し、各例外がスローされる条件をjavadocの@throwsタグを使用して正確に文書化してください
+ * メソッドがスローする可能性がある個々のチェックされない例外を文書化するために、javadocの@throwsタグを使用してください。
+ * しかし、メソッド宣言にチェックされない例外を含めるために、throws予約語を使用してはいけません。
+ *
+ * @author sinokuma
+ */
+public class Sample {
+    // nothing to do
+}
